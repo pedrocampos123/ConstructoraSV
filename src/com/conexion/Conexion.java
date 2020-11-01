f8c0253d3111e8f62fa9770d54b93e1eddb89c04
luis- <luis-@LAPTOP-5SJF0JRK>
2020-10-31 22:41:08 -0600

    Borrando prueba3

--- src/com/conexion/Conexion.java
+++ src/com/conexion/Conexion.java
@@ -20,6 +20,7 @@ public class Conexion {
     
     private Connection con = null;
     String prueba = "solamente es una varible de prueba";
+    String prueba3= "Es la tercera prueba";
 
     public Connection getCon() {
         return con;
