4060502c01f9399a782914d96580022e4c9a702a
pedrocampos123 <pedroarturocampos@gmail.com>
2020-11-01 00:18:05 -0600

    CORRECCION

--- src/com/conexion/Conexion.java
+++ src/com/conexion/Conexion.java
@@ -19,6 +19,8 @@ import javax.swing.JOptionPane;
 public class Conexion {
     
     private Connection con = null;
+    String prueba = "solamente es una varible de prueba"; 
+    int cero = 0;
 
     public Connection getCon() {
         return con;
